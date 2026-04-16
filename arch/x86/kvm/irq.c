e19866977107744dfb42011c65faf057cbea8f17
Joerg Roedel <joerg.roedel@amd.com>
2026-04-16 14:23:53 +0000

    kvm: Add plane_level to kvm_kernel_irq_routing_entry
    Track the target plane-level of MSI irqs in struct
    kvm_kernel_irq_routing_entry. This will be used to send MSI IRQs to
    the right plane-level once planes are supported.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- arch/x86/kvm/irq.c
+++ arch/x86/kvm/irq.c
@@ -332,6 +332,7 @@ int kvm_set_routing_entry(struct kvm *kvm,
 		e->msi.address_lo = ue->u.msi.address_lo;
 		e->msi.address_hi = ue->u.msi.address_hi;
 		e->msi.data = ue->u.msi.data;
+		e->msi.plane_level = 0;
 
 		if (kvm_msi_route_invalid(kvm, e))
 			return -EINVAL;
