69b847fb73dc423a0d0ec849365182670bc697fe
Joerg Roedel <joerg.roedel@amd.com>
2026-05-04 13:08:57 +0000

    kvm: Allocate struct kvm_plane in architecture code
    Move plane allocation to architecture code so that per-arch
    implementations can embed the structure into another one for keeping
    additional data.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- arch/mips/kvm/mips.c
+++ arch/mips/kvm/mips.c
@@ -120,6 +120,19 @@ unsigned kvm_arch_max_planes(struct kvm *kvm)
 	return 1;
 }
 
+struct kvm_plane *kvm_alloc_plane(void)
+{
+	/* For better type checking, do not return kzalloc() value directly */
+	struct kvm_plane *plane = kzalloc(sizeof(*plane), GFP_KERNEL_ACCOUNT);
+
+	return plane;
+}
+
+void kvm_free_plane(struct kvm_plane *plane)
+{
+	kfree(plane);
+}
+
 bool kvm_arch_vcpu_in_kernel(struct kvm_vcpu *vcpu)
 {
 	return false;
